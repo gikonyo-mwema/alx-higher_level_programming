2595443bdcb048bb42b2f104765fd3432208bf12
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-04 14:34:14 -0800

    check blank entry

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i, num in enumerate(row):
-            if i != len(row) - 1:
-                print("{:d}".format(num), end=" ")
-            else:
-                print("{:d}".format(num))
+    if not matrix:
+        print()
+    else:
+        for row in matrix:
+            for i, num in enumerate(row):
+                if i != len(row) - 1:
+                    print("{:d}".format(num), end=" ")
+                else:
+                    print("{:d}".format(num))
