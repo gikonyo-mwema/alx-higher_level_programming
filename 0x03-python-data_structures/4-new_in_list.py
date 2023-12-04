aaeb7e25f14cbb1ff2b5c9d9918a2e09c02be1e4
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-04 11:09:22 -0800

    return original

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,8 @@
 
 def new_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
+        return my_list[:]
+    else:
         copy = my_list[:]
         copy[idx] = element
         return copy
