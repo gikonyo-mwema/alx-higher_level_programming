92beb2a80293fb056082f39d6afd194757ddb5bd
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-06 15:33:19 -0800

    weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum_score, sum_weight = sum(score * weight for score, weight in my_list), sum(weight for _, weight in my_list)
+    return sum_score / sum_weight if sum_weight else 0
