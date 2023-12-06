abd91ba5e541137dd4604d92cf4838416ef1cc15
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-06 15:38:10 -0800

    fixed indentation

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,5 +2,7 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    sum_score, sum_weight = sum(score * weight for score, weight in my_list), sum(weight for _, weight in my_list)
+    sum_score = sum(score * weight for score, weight in my_list)
+    sum_weight = sum(weight for _, weight in my_list)
     return sum_score / sum_weight if sum_weight else 0
+
