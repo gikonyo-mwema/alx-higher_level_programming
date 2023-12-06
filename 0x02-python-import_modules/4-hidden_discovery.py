4a4366db231e7e1171af7f516d233d743991040f
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-06 11:42:27 -0800

    names

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+import hidden_4
+
+
+def print_names():
+    n = dir(hidden_4)
+    for n in sorted(names):
+        if not n.startswith('__'):
+            print(name)
+
+
+if __n__ == "__main__":
+    print_names()
