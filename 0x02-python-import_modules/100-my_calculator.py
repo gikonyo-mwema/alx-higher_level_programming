52817e829cbdad6dd09b1d229cbf7b8022d0434a
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-06 12:12:04 -0800

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    sign = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if sign == '+':
+        result = add(a, b)
+    elif sign == '-':
+        result = sub(a, b)
+    elif sign == '*':
+        result = mul(a, b)
+    elif sign == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(a, sign, b, result))
+
+if __name__ == "__main__":
+    main()
