f7ab8930185c8d8a7224e6d9252e2f986dd5e7f8
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-06 15:54:53 -0800

    weight avg

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,9 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    sum_score = sum(score * weight for score, weight in my_list)
-    sum_weight = sum(weight for _, weight in my_list)
-    return sum_score / sum_weight if sum_weight else 0
-
+    total_weight = 0
+    total_score = 0
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+    return total_score / total_weight if total_weight else 0
