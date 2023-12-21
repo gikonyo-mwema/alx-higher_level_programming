68cd0d6465e95858e7e6df1911e54769f801a1a1
gikonyo-mwema <gikonyo.mwema@gmail.com>
2023-12-20 17:00:27 -0800

    second square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module for a square"""
+
+
+class Square:
+    """
+    This class represents a square.
+    It has a private instnce attribut: size.
+    """
+    def __init__(self, size):
+        """
+        Initializes the square.
+        Args: 
+            size (int): The size of the square.
+        """
+        self.__size = size  
