c01f8b89f110a121bac5f9168b9ee6329fc23065
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-05 18:45:35 -0800

    indentation fixed

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Represent a rectangle."""
-    
+
     number_of_instances = 0
     print_symbol = "#"
 
@@ -59,7 +59,8 @@ class Rectangle:
         """Return a string representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.__width for _ in range(self.__height))
+        return "\n".join(str(self.print_symbol) * self.__width for _ in
+                         range(self.__height))
 
     def __repr__(self):
         """Return a string representation of the rectangle for reproduction."""
