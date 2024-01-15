a21e863627baf7863c1bb684226593f6ca7599fc
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-15 03:13:53 -0800

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This is the "base" module.
+
+The base module supplies one class, Base. For example,
+
+>>> b = Base(1)
+>>> b.id
+1
+"""
+
+class Base:
+    """Base model.
+
+    This represents the 'base' for all other classes.
+    
+    Private Class Attributes:
+        _nb_objects (int): Number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
