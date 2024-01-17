768611b95e63ff8fb6c69365b42a1acad15a3e08
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-16 19:03:13 -0800

    format

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
 def lookup(obj):
     """
-    Function to get a list of available attributes and methods of an object.
-
-    Parameters:
-    obj (any): The object whose attributes and methods are to be returned.
-
     Returns:
     list: A list of strings representing the attributes and methods of the object.
     """
