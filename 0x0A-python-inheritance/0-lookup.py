2e196548912bcd12e99b63ff120fc56f24d27892
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-16 19:07:46 -0800

    short docstring'

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,9 +3,6 @@
 
 
 def lookup(obj):
-    """
-    Returns:
-    list: A list of strings representing the attributes 
-    and methods of the object.
+    """Returns a list of attributes and methods of the object.
     """
     return dir(obj)
