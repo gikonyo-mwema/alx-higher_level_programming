8b5b78ea9957b6de59733ad600f7e2e3b60e0ef2
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-16 19:05:12 -0800

    format style

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Defines an object attribute lookup function."""
 
+
 def lookup(obj):
     """
     Returns:
-    list: A list of strings representing the attributes and methods of the object.
+    list: A list of strings representing the attributes 
+    and methods of the object.
     """
     return dir(obj)
-
