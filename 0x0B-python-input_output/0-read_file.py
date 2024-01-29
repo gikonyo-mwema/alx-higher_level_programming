5856a5746cdc42ef108cd48fdf966e4d7a040d34
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-01-28 17:46:07 -0800

    reads text

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defiens a function to read a file and print its contents."""
+
+
+def read_file(filename=""):
+    """Open the file in read mode with UTF-8 encoding and print content."""
+    with open(filename, "r", encoding="utf8") as f:
+        print(f.read())
