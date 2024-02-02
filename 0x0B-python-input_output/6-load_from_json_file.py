d816541286a3ed860e10826abcfe297cc32fee64
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-01 23:01:07 -0800

    style pass

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,9 +2,11 @@
 """ Creates a python object form JSON """
 import json
 
+
 def load_from_json_file(filename):
     """
-    This function takes a filename and creates a Python object from tthe JSON representation in the file.
+    This function takes a filename and creates a Python object
+    from tthe JSON representation in the file.
 
     Args:
         filename (str): The name of the file to read from.
