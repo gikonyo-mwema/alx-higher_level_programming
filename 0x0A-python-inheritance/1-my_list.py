bf9569fd3129f49a5e79799b8f041eb60aa92627
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-02 20:20:23 -0800

    inherits form list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""Defines a MyList class that inherits from list."""
+
+
 class MyList(list):
+    """A class that inherits form list and includes a method to print"""
+
     def print_sorted(self):
+        """Prints the list in ascending order."""
         print(sorted(self))
