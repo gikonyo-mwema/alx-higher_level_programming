64ef62287ba81eaad47447a81b6677a5a729df02
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-02 20:54:34 -0800

    style check

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Defines a function to check if an object is an instance of a class that inherited from the specified class."""
+"""Defines a function to check if an object is an instance of a class
+    that inherited from the specified class.
+"""
+
 
 def inherits_from(obj, a_class):
-    """Check if an object is an instance of a class that inherited from the specified class.
+    """Check if an object is an instance of a class that inherited
+    from the specified class.
 
     Args:
         obj: The object to check.
         a_class: The class to check against.
 
     Returns:
-        True if the object is an instance of a class that inherited from the specified class, otherwise False.
+        True if the object is an instance of a class that inherited from
+        the specified class, otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
