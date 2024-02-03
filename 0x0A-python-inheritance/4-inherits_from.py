d24051766ae046a952753524d1cb69d8f75e458d
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-02 20:51:43 -0800

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function to check if an object is an instance of a class that inherited from the specified class."""
+
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of a class that inherited from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        True if the object is an instance of a class that inherited from the specified class, otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
