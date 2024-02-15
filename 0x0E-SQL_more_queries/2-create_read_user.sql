8db816cfb32fe1431b1792b49f560fd04f0689a3
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-14 17:39:47 -0800

    check and create

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Script to create user with SELECT privilege
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
