3a350256ff229b797cbae5da4af7e5e600869e9c
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-02-16 10:37:03 -0800

    records >= 10

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- Script to list all record with score greater or equal to 10
-SELECT name, score
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
