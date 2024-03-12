ba1996f7cdef25bca308e9fcfde3602ed2f0db55
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-11 19:48:16 -0700

    squares

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const size = parseInt(args[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
