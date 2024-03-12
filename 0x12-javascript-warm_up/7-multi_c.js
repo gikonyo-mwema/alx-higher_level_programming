2529b673e5e3730c5089c52a39ef4dfdda2c2a9e
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-11 19:36:50 -0700

    format fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,7 @@ const x = parseInt(args[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i > x; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
-
