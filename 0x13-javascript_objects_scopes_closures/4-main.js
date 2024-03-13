63bee6040013a703f17a12fdcd145ec19eaf129a
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-12 19:24:20 -0700

    3 functions

--- 0x13-javascript_objects_scopes_closures/4-main.js
+++ 0x13-javascript_objects_scopes_closures/4-main.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const Rectangle = require('./4-rectangle');
 
-const s1 = new Square(4);
-s1.print();
-s1.double();
-s1.print();
+const r1 = new Rectangle(2, 3);
+console.log('Normal:');
+r1.print();
+
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
+r1.print();
