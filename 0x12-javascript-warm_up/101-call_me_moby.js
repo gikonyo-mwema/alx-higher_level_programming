1eaf3e4793310382ab078914429252692df0aacd
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-13 09:12:35 -0700

    scope fixed

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-exports.callMeMoby = function (repeat, theFunction) {
-  for (let i = 0; i < repeat; i++) {
-    theFunction();
-  }
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
 };
