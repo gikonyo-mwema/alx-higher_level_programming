118f47db19af845e83000036eba7e1fdf49b73db
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-03-16 19:12:26 -0700

    sorting by list comprehension

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,18 +7,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    query = (
-        "SELECT * FROM `states` "
-        "WHERE `name` LIKE 'N%' "
-        "ORDER BY `id` ASC"
-    )
-    cursor.execute(query)
-    for state in cursor.fetchall():
-        print(state)
+    cursor.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in cursor.fetchall() if state[1][0] =="N"]
