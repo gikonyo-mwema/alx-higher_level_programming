c6bfedaf53ecd7f5ccce00f7c7840eed6b7c62de
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-04-24 05:14:55 +0300

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  const tasks = JSON.parse(body);
+  const completedTasks = {};
+
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (completedTasks[task.userId]) {
+        completedTasks[task.userId]++;
+      } else {
+        completedTasks[task.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasks);
+});
